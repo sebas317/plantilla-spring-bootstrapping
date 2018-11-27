996e2b0db9119af7b0d79c60f0495ea0cf040106
sebas317 <sebasortiz317@hotmail.com>
2018-11-27 00:56:22 -0500

    commit inicial

---
+++ rating-service/src/main/java/com/creativetics/ratingService/entity/Rating.java
@@ -0,0 +1,40 @@
+package com.creativetics.ratingService.entity;
+
+public class Rating {
+
+	private Long id;
+	private Long bookId;
+	private int stars;
+
+	public Rating(Long id, Long bookId, int stars) {
+		super();
+		this.id = id;
+		this.bookId = bookId;
+		this.stars = stars;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
+	}
+
+	public int getStars() {
+		return stars;
+	}
+
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+
+}
